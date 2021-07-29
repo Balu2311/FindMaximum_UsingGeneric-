5787d34ff7d71fdf12813c4b38e47b5c56c1d088
Vallem Balu <vbnreddy5@gmail.com>
2021-07-29 17:45:46 +0530

    Using generic finding maxValue.

--- FindMaximumValueTesting/UnitTest1.cs
+++ FindMaximumValueTesting/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace FindMaximumValueTesting
         [TestMethod]
         public void GivenMaxNumber_FirstPosition()
         {
-            int result = MaximumNumberCheck.MaximumIntegerNumber(40, 20, 30);
+            int result = MaximumNumberCheck.findMaximum(40, 20, 30);
             int maxNumber = 40;
             Assert.AreEqual(maxNumber, result);
         }
@@ -18,7 +18,7 @@ namespace FindMaximumValueTesting
         [TestMethod]
         public void GivenMaxNumber_SecondPosition()
         {
-            int result = MaximumNumberCheck.MaximumIntegerNumber(10, 40, 30);
+            int result = MaximumNumberCheck.findMaximum(10, 40, 30);
             int maxNumber = 40;
             Assert.AreEqual(maxNumber, result);
         }
@@ -26,7 +26,7 @@ namespace FindMaximumValueTesting
         [TestMethod]
         public void GivenMaxNumber_ThirdPosition()
         {
-            int result = MaximumNumberCheck.MaximumIntegerNumber(10, 20, 40);
+            int result = MaximumNumberCheck.findMaximum(10, 20, 40);
             int maxNumber = 40;
             Assert.AreEqual(maxNumber, result);
         }
@@ -35,21 +35,21 @@ namespace FindMaximumValueTesting
         [TestMethod]
         public void GivenMaxFloatNumber_FirstPosition()
         {
-            double result = MaximumNumberCheck.MaximumFloatNumber(55.5, 20.2, 33.3);
+            double result = MaximumNumberCheck.findMaximum(55.5, 20.2, 33.3);
             double maxNumber = 55.5;
             Assert.AreEqual(maxNumber, result);
         }
         // Given the maximum float number second position.
         public void GivenMaxFloatNumber_SecondPosition()
         {
-            double result = MaximumNumberCheck.MaximumFloatNumber(20.2, 55.5, 33.3);
+            double result = MaximumNumberCheck.findMaximum(20.2, 55.5, 33.3);
             double maxNumber = 55.5;
             Assert.AreEqual(maxNumber, result);
         }
         // Given the maximum float number third position.
         public void GivenMaxFloatNumber_ThirdPosition()
         {
-            double result = MaximumNumberCheck.MaximumFloatNumber(20.2, 33.3, 55.5);
+            double result = MaximumNumberCheck.findMaximum(20.2, 33.3, 55.5);
             double maxNumber = 55.5;
             Assert.AreEqual(maxNumber, result);
         }
@@ -57,21 +57,21 @@ namespace FindMaximumValueTesting
         // Given the maximum string first position.
         public void GivenMaxString_FirstPosition()
         {
-            string result = MaximumNumberCheck.MaximumString("Peach", "Apple", "Banana");
+            string result = MaximumNumberCheck.findMaximum("Peach", "Apple", "Banana");
             string maxString = "Peach";
             Assert.AreEqual(maxString, result);
         }
         // Given the maximum string second position.
         public void GivenMaxString_SecondPosition()
         {
-            string result = MaximumNumberCheck.MaximumString("Apple", "Peach", "Banana");
+            string result = MaximumNumberCheck.findMaximum("Apple", "Peach", "Banana");
             string maxString = "Peach";
             Assert.AreEqual(maxString, result);
         }
         // Given the maximum string third position.
         public void GivenMaxString_ThirdPosition()
         {
-            string result = MaximumNumberCheck.MaximumString("Apple", "Banana", "Peach");
+            string result = MaximumNumberCheck.findMaximum("Apple", "Banana", "Peach");
             string maxString = "Peach";
             Assert.AreEqual(maxString, result);
         }
--- FindMaximumValue_UsingGeneric/MaximumNumberCheck.cs
+++ FindMaximumValue_UsingGeneric/MaximumNumberCheck.cs
@@ -6,58 +6,28 @@ namespace FindMaximumValue_UsingGeneric
 {
     public class MaximumNumberCheck
     {
-        //Find Maxmum intiger value
-        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        public static T findMaximum<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
         {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
             {
                 return firstValue;
             }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
             {
                 return secondValue;
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
             {
                 return thirdValue;
             }
-            throw new Exception("First,Second and Third Values Same...");
-        }
-        //Find Maxmum Float value
-        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
-            throw new Exception("First,Second and Third Values Same...");
-        }
+            throw new Exception("firstValue,secondValue,thirdValue are same");
 
-        //Find Maxmum string 
-        public static string MaximumString(string firstString, string secondString, string thirdString)
-        {
-            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0)
-            {
-                return firstString;
-            }
-            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0)
-            {
-                return secondString;
-            }
-            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0)
-            {
-                return thirdString;
-            }
-            throw new Exception("First,Second and Third String Same...");
         }
-
     }
 }
